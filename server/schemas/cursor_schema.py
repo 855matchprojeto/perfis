73f18ea13d2c64026f856412c75f3e38b35be9bf
Gustavo Libraiz <gulibraiz@gmail.com>
2022-04-23 21:34:18 -0300

    Corrigindo previous_cursor na lógica de paginação

--- server/schemas/cursor_schema.py
+++ server/schemas/cursor_schema.py
@@ -1,11 +1,13 @@
 from pydantic import BaseModel
-from typing import Any
+from typing import Any, Optional
 
 
 class Cursor(BaseModel):
 
+    previous_encoded_cursor: Optional[str]
     sort_field_key: str
     sort_field_type: str
     operator: str
     value: Any
+    id: int
 
